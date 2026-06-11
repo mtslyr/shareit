cd941f2adb15a3fda07eedd5e643362d7fbc410c
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2026-06-11 11:07:46 +0500

    checkstyle

--- src/main/java/ru/practicum/shareit/item/comment/repository/CommentStorage.java
+++ src/main/java/ru/practicum/shareit/item/comment/repository/CommentStorage.java
@@ -9,6 +9,8 @@ import java.util.Set;
 
 @Repository
 public interface CommentStorage extends JpaRepository<Comment, Long> {
+
     List<Comment> findByItemId(Long itemId);
+
     List<Comment> findByItemIdIn(Set<Long> itemIds);
 }
